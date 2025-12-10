5d81a2473c2990541764aadae4e10aa4965eb854
benjibyte <benjipixel@proton.me>
2025-12-10 12:30:02 -0600

    Pushing local project to github; slight edits for the generated readme from the repo's description

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# color-sampler
+# Color Pallet Creator
+
 I love to paint on Procreate and Krita, but myself and my mother were facing issues with the default behavior of how Procreate imports colors from an image. So I decided to make this little piece of software for her birthday. The color scheme of the website used the Everforest pallet made by Sainhe.
+
+## The Web app for this software can be found at
+
+<url here>
+
+
---
+++ script.js
@@ -0,0 +1,32 @@
+function uploadImage() {
+    // allow the user to select an image from their computer and
+    // display a smaller version of it on the HTML document as an img tag
+    // with an ID of "uploaded-image"
+
+
+}
+
+function divideImage() {
+    // take the new image tag with the id of "uploaded-image" and get it's
+    // height, width, and a number it is divisible by.
+
+    // Divide the image into little quadrants of color values from each quadrant (perhaps store in json?)
+    
+    // return an array of divided json objects containing the divided colors in number format (rgb or HEX color codes)
+}
+
+function getColors() {
+    // Take the array of divided json colors, and get the average color of each quadrant. 
+    // If there are 2 or more colors within a certain range of similarness, then either merge or delete the new one
+    
+    // Then create a new img tag and draw 
+    // each color in little boxes next to each other on the new tag until all the average colors have been taken.
+
+    // return state: place a new image tag that the User can see, containing all the average colors. This is the pallet to download. 
+    // .... Also, turn the download button's CSS bright blue and enable the button to be clicked.
+}
+
+function downloadPalete() {
+    // Get the new pallete img tag and download it to the computer with the Download palete button is clicked. as a .jpg file.
+    // Send an Alert() that it was a succesful download!
+}
