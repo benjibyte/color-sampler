7c0020be368a80fd55f7744c4c6dfbc011bd8218
benjibyte <benjipixel@proton.me>
2025-12-29 18:00:20 -0600

    fixed more typos

--- scripts/app.js
+++ scripts/app.js
@@ -119,17 +119,6 @@ function divideImage() {
       const row2SourceY = heightDivisor;
       const row3SourceY = heightDivisor * 2;
 
+    }
   }
-}
-function getColors() {
-  // Take the array of divided json colors, and get the average color of each quadrant.
-  // If there are 2 or more colors within a certain range of similarness, then either merge or delete the new one
-  // Then create a new img tag and draw
-  // each color in little boxes next to each other on the new tag until all the average colors have been taken.
-  // return state: place a new image tag that the User can see, containing all the average colors. This is the pallet to download.
-  // .... Also, turn the download button's CSS bright blue and enable the button to be clicked.
-}
-function downloadPalete() {
-  // Get the new pallete img tag and download it to the computer with the Download palete button is clicked. as a .jpg file.
-  // Send an Alert() that it was a succesful download!
 }
